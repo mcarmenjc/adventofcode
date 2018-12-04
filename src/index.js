93a07b7c275fd86520b4866c9411e1325c5e32ac
MariCarmen Jimenez Campos <cammria@amazon.com>
2018-12-04 11:33:43 +0000

    day 1 finished

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//ADVENT OF CODE 2018
+
+import { day01 } from './day01';
+
+function header(){
+    console.log("-------------------------------------------------");
+    console.log("               ADVENT OF CODE 2018");
+    console.log("-------------------------------------------------");
+}
+
+header();
+day01();
