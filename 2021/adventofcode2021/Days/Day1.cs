a90968fe438fdd1b87c950413dc24ede3ac713f1
majime <majime@microsoft.com>
2021-12-01 12:58:18 +0000

    2021 day1

---
+++ 2021/adventofcode2021/Days/Day1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace adventofcode2021.Days
+{
+    public class Day1
+    {
+        private IList<int> _depths = new List<int>();
+
+        public Day1()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@".\Inputs\day1.txt");
+
+            foreach(string line in lines)
+            {
+                int depth = Int32.Parse(line);
+                _depths.Add(depth);
+            }
+        }
+
+        public int GetNumberOfTimesMeasurementIncreases()
+        {
+            int increases = 0;
+            
+            for (int i = 0; i < _depths.Count - 1; i++)
+            {
+                if (_depths[i] < _depths[i + 1])
+                {
+                    increases++;
+                }
+            }
+            return increases;
+        }
+
+        public int GetNumberOfTimesSumOfMeasurementsInSlidingWindowIncreases()
+        {
+            int windowSize = 3;
+            int increases = 0;
+            int currentWindow = 0;
+            
+            for (int i = 0; i < _depths.Count; i++)
+            {
+                if (i < windowSize)
+                {
+                    currentWindow += _depths[i];
+                }
+                else
+                {
+                    int nextWindow = currentWindow - _depths[i - windowSize] + _depths[i];
+
+                    if (currentWindow < nextWindow)
+                    {
+                        increases++;
+                    }
+                    currentWindow = nextWindow;
+                }
+            }
+
+            return increases;
+        }
+
+        public static void Print()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("                      ~   ~  ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("~");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(" ~");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("~ ~~");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("~");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("~~~");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("~");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("~~");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("~");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("~");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("~~");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("~");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("~  ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" 1 ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("**");
+        }
+    }
+}
---
+++ 2021/adventofcode2021/Program.cs
@@ -0,0 +1,21 @@
+﻿using adventofcode2021.Days;
+using System;
+
+namespace adventofcode2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("                  .-*_* CALENDAR *_*-.                  ");
+            Console.WriteLine();
+            Day1.Print();
+            Console.WriteLine();
+
+            Console.WriteLine("Day 1");
+            Day1 day1 = new Day1();
+            Console.WriteLine($"Part 1 = {day1.GetNumberOfTimesMeasurementIncreases()}");
+            Console.WriteLine($"Part 2 = {day1.GetNumberOfTimesSumOfMeasurementsInSlidingWindowIncreases()}");
+        }
+    }
+}
